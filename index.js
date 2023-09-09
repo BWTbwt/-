75a1b2ea0577e63cac41c8f9f9897ec61867b154
NARUTO <12106854+ba-wenting@user.noreply.gitee.com>
2023-09-09 12:16:09 +0800

    项目四：奔跑的汽车

--- index.js
+++ index.js
@@ -1,177 +1,181 @@
-// 弹窗
-let selectDialog = document.querySelector('#select-dialog')
-// 开始下一回合按钮
-let startThisRoundBtn = document.querySelector('#start-this-round-btn')
-// form 表单
-let selectForm = document.querySelector('#select-form')
-// 选择框
-let gestureSelect = document.querySelector('#gesture-select')
-// 结果渲染
-let resultThisround = document.querySelector('.result-of-this-round')
+// 获取游戏区域
+let gameMap = document.querySelector('.game')
+// 获取车的图片
+let carDom = document.querySelector('.car')
+// 车的类型
+let carType = document.querySelector('#carType')
+// 速度
+let speedSelect = document.querySelector('#speed')
 
-// 手势图片的枚举
-const GestureImg = {
-  rock: './images/rock.png',
-  scissor: './images/scissor.png',
-  paper: './images/paper.png',
+// 车型对应的图片
+const carTypeImg = {
+  tractors: './image/car1.png',
+  truck: './image/car2.png',
+  motor: './image/car3.png',
+  roadster: './image/car4.png',
+  electromobile: './image/car5.png',
+  bike: './image/car6.png',
 }
-// 名称的枚举
-const Name = {
-  computer: '机器人',
-  my: '玩家',
-}
-// 定义手势数组
-const Gesture = ['rock', 'scissor', 'paper']
-// 总回合数
-const MaxRoundNum = 3
-let roundIndex = 1
-
-// 电脑和玩家的输赢情况
-const comScore = {
-  win: '0',
-  lose: '0',
-}
-const myScore = {
-  win: '0',
-  lose: '0',
+// 上下左右键对应的keycode值
+const KeyCode = {
+  left: 37,
+  up: 38,
+  right: 39,
+  down: 40,
 }
 
-// 展示弹窗
+let carHeadDirection = 'right'
+
 function initGame() {
-  // 点击 开始下一回合，出现弹窗
-  startThisRoundBtn.addEventListener('click', () => {
-    selectDialog.showModal()
+  // 监听事件 -- 键盘
+  keyDirection()
+  // 监听事件 -- 车的类型
+  carChangeType()
+  // 监听事件 -- 速度的变化
+  speedChange()
+  // 从localStorage中读取之前保存的数据，取出来时是字符串，需要用JSON.parse解析一下
+  // 如果什么都不存的话，取出来是null
+  const data = JSON.parse(localStorage.getItem('allCar'))
+  if (data) {
+    speedSelect.value = data.speed
+    carType.value = data.carType
+    carDom.style.left = data.position.left
+    carDom.style.top = data.position.top
+    renderCar(data.carType)
+    carChangeHeadDirection(data.headCar)
+  }
+}
+
+// 监听键盘事件 对应的上下左右键
+function keyDirection() {
+  window.addEventListener('keydown', function (e) {
+    const keyCode = e.keyCode
+    console.log(keyCode)
+    moveCar(keyCode)
+    storageAll()
   })
-  // 选中选项后，点击确定关闭弹窗
-  selectForm.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const mygesture = gestureSelect.value
-    renderMyGesture(mygesture)
-    selectDialog.close()
-    const comGesture = renderComGesture()
-    console.log(comGesture, mygesture)
-    judgeWinlose(comGesture, mygesture)
+}
+
+// 监听车的类型
+function carChangeType() {
+  carType.addEventListener('change', function (e) {
+    renderCar(e.target.value)
+    storageAll()
   })
+}
+// 监听速度的变化
+function speedChange() {
+  speedSelect.addEventListener('change', function (e) {
+    storageAll()
+  })
+}
+
+// 切换车对应的类型
+function renderCar(carType) {
+  const img = carTypeImg[carType]
+  carDom.src = img
+}
+
+// 移动汽车
+function moveCar(keyCode) {
+  // 修改汽车dom的style，抽象成方法
+  // 如果是往左或者右移动，那么只需要修改绝对定位的left属性即可
+  // 往右移动是增加left的值，往左移动是减少left的值
+  // 往上移动是减少top的值，往下移动是增加top的值
+  function _move(direction) {
+    // window.getComputedStyle 是一个 JavaScript 方法，用于获取指定元素的计算样式。
+    const computedStyle = window.getComputedStyle(carDom)
+    // 获取速度的值
+    const curSpeed = parseInt(speedSelect.value)
+
+    let valtWithPX, valWithNumber
+    // 移动后最新的left和top值，待计算
+    let newLeft = 0,
+      newTop = 0
+    if (direction === 'left' || direction === 'right') {
+      valtWithPX = computedStyle.left
+      // 截取 索引0 到 索引（排除px）
+      // 例如，假设 valtWithPX 的值为 "100px"，那么 valtWithPX.length 的结果为 5。因此，表达式 valtWithPX.slice(0, valtWithPX.length - 2) 将返回从索引 0 到索引 3（不包括索引 3）的子字符串，即 "100"。
+      valWithNumber = parseInt(valtWithPX.slice(0, valtWithPX.length - 2))
+      if (direction === 'left') {
+        newLeft = valWithNumber - curSpeed
+      } else {
+        newLeft = valWithNumber + curSpeed
+      }
+      carDom.style.left = `${newLeft}px`
+    } else {
+      valtWithPX = computedStyle.top
+      valWithNumber = parseInt(valtWithPX.slice(0, valtWithPX.length - 2))
+      if (direction === 'up') {
+        newTop = valWithNumber - curSpeed
+      } else {
+        newTop = valWithNumber + curSpeed
+      }
+      carDom.style.top = `${newTop}px`
+    }
+  }
+  // 键盘的方向
+  let direction
+  switch (keyCode) {
+    case KeyCode.left:
+      direction = 'left'
+      break
+    case KeyCode.up:
+      direction = 'up'
+      break
+    case KeyCode.right:
+      direction = 'right'
+      break
+    case KeyCode.down:
+      direction = 'down'
+      break
+    default:
+      break
+  }
+  _move(direction)
+  carChangeHeadDirection(direction)
+}
+
+// 改变车头的方向，这里我们使用修改类名的方式来做，提前把各个方向的css类名写好
+function carChangeHeadDirection(direction) {
+  carHeadDirection = direction
+  carDom.classList.remove('car-up', 'car-down', 'car-left', 'car-right')
+  switch (direction) {
+    case 'up':
+      carDom.classList.add('car-up')
+      break
+    case 'down':
+      carDom.classList.add('car-down')
+      break
+    case 'left':
+      carDom.classList.add('car-left')
+      break
+    case 'right':
+      carDom.classList.add('car-right')
+      break
+    default:
+      break
+  }
+}
+
+// 保存当前最新的速度、车型、车头方向、车的位置到localhost
+function storageAll() {
+  const curSpeed = speedSelect.value
+  const carChangeType = carType.value
+  const positionLeft = carDom.style.left
+  const positionTop = carDom.style.top
+
+  const objCar = {
+    speed: curSpeed,
+    carType: carChangeType,
+    position: {
+      left: positionLeft,
+      top: positionTop,
+    },
+    headCar: carHeadDirection,
+  }
+
+  localStorage.setItem('allCar', JSON.stringify(objCar))
 }
 
 initGame()
-
-// 玩家：选中选项后，点击确定，手势动画会根据选项变化
-function renderMyGesture(myoption) {
-  // 获取 玩家的手势img 标签
-  let myGestureImg = document.querySelector('.my .gesture')
-  // 获取传入的 选项，变为对应的图片
-  let theGesture = GestureImg[myoption]
-  myGestureImg.innerHTML = ` <img src="${theGesture}" >`
-}
-
-// 机器人：随机显示图片
-function renderComGesture() {
-  // 产生0-2的随机数，来对应石头 剪刀 布
-  const random = Math.floor(Math.random() * 3)
-  // 获取 机器人的手势img 标签
-  let comGestureImg = document.querySelector('.computer .gesture')
-  // 获取传入的 选项，变为对应的图片
-  let randomImg = Gesture[random]
-  let theGesture = GestureImg[randomImg]
-  comGestureImg.innerHTML = `<img src="${theGesture}" >`
-
-  return randomImg
-}
-
-// 机器人和玩家的输赢数据
-function renderResult() {
-  let comResult = document.querySelector('.computer .score')
-  comResult.innerHTML = `胜利：${comScore.win} ｜ 负：${comScore.lose}`
-  let myResult = document.querySelector('.my .score')
-  myResult.innerHTML = `胜利：${myScore.win} ｜ 负：${myScore.lose}`
-}
-
-// 渲染汇合数
-function renderRoundInfo(current, total) {
-  document.querySelector(
-    '.round'
-  ).innerHTML = `第${current}回合（共${total}回合）`
-}
-
-// 判断输赢
-function judgeWinlose(computer, my) {
-  // 先定义结果文字
-  let theroundResult = ''
-  // 玩家 -- 赢
-  let Ismywin = true
-  if (computer === my) {
-    theroundResult = '本回合平局'
-  } else {
-    // 如果机器 出石头
-    if (computer === 'rock') {
-      if (my === 'paper') {
-        Ismywin = true
-      } else {
-        Ismywin = false
-      }
-    }
-    // 如果机器 出剪刀
-    if (computer === 'scissor') {
-      if (my === 'rock') {
-        Ismywin = true
-      } else {
-        Ismywin = false
-      }
-    }
-    // 如果机器 出布
-    if (computer === 'paper') {
-      if (my === 'scissor') {
-        Ismywin = true
-      } else {
-        Ismywin = false
-      }
-    }
-    // 如果Ismywin为true，则文字渲染为玩家赢
-    if (Ismywin) {
-      theroundResult = '本回合玩家赢了'
-      myScore.win++
-      comScore.lose++
-    } else {
-      theroundResult = '本回合机器人赢了'
-      myScore.lose++
-      comScore.win++
-    }
-  }
-  resultThisround.innerHTML = theroundResult
-  renderResult()
-  renderRoundInfo(roundIndex, MaxRoundNum)
-
-  if (roundIndex === MaxRoundNum) {
-    if (comScore.win > myScore.win) {
-      finishGame('computer')
-    } else if (comScore.win < myScore.win) {
-      finishGame('my')
-    } else {
-      finishGame()
-    }
-  } else {
-    if (comScore.win === 2) {
-      finishGame('computer')
-      return
-    } else if (myScore.win === 2) {
-      finishGame('my')
-      return
-    }
-    roundIndex++
-  }
-}
-
-// 结束回合后，开始按钮隐藏
-// 如果传入的参数是 my，代表玩家赢了；如果是computer，代表机器人赢了
-// 如果什么都不传，表示平局
-function finishGame(winer) {
-  selectDialog.close()
-  startThisRoundBtn.style.display = 'none'
-  let final = document.querySelector('.result-of-final')
-  if (!winer) {
-    final.innerHTML = `二位，是平局哦`
-  } else {
-    final.innerHTML = `胜利者是${Name[winer]},恭喜!!!`
-  }
-}
